f78014c9bdfa38f066ce1d6cd8c81ba67fbe3db3
Vũ Ngọc Tú K14_FPL HN <tuvnph08581@fpt.edu.vn>
2020-08-15 15:40:02 +0700

    Updates

---
+++ precache-manifest.4d16c4d4f40de162cdb291c489492ead.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be358b4fcfce7e3776e2ad93242431f",
+    "url": "/Assignment-React/index.html"
+  },
+  {
+    "revision": "19f5f49aa3df917c7d77",
+    "url": "/Assignment-React/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "4a0e80e54807494aca26",
+    "url": "/Assignment-React/static/css/main.ebe42311.chunk.css"
+  },
+  {
+    "revision": "19f5f49aa3df917c7d77",
+    "url": "/Assignment-React/static/js/2.567f87a2.chunk.js"
+  },
+  {
+    "revision": "5e8a93876523ce3662c437e5fe082aeb",
+    "url": "/Assignment-React/static/js/2.567f87a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0e80e54807494aca26",
+    "url": "/Assignment-React/static/js/main.c854e374.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/Assignment-React/static/js/main.c854e374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419c58162cfe6217fc50",
+    "url": "/Assignment-React/static/js/runtime-main.572e0aa5.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/Assignment-React/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/Assignment-React/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/Assignment-React/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/Assignment-React/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/Assignment-React/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
